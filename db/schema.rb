6188f96ce8cf3f72df8feca470a8c352cd27f8b7
Heber <heberlazo@gmail.com>
2021-02-24 16:22:45 -0500

    Add scaffold message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_201500) do
+ActiveRecord::Schema.define(version: 2021_02_24_212156) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
