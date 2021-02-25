c1c19262476b0ed842d5445b22965b0ad16896f5
Huemac Alfredo Córdova <hmca101292@outlook.com>
2021-02-25 15:05:55 -0600

    Delete all the all commented lines as requested by TSE

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,28 +2,21 @@ class MessagesController < ApplicationController
   before_action :set_message, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[index show]
 
-  # GET /messages or /messages.json
   def index
     @messages = Message.all.order('created_at DESC')
     @message = Message.new
   end
 
-  # GET /messages/1 or /messages/1.json
   def show; end
 
-  # GET /messages/new
   def new
-    # @message = Message.new
-    @message = current_user.messages.build # para interconectarlos
+    @message = current_user.messages.build
   end
 
-  # GET /messages/1/edit
   def edit; end
 
-  # POST /messages or /messages.json
   def create
-    # @message = Message.new(message_params)
-    @message = current_user.messages.build(message_params) # para interconectarlos
+    @message = current_user.messages.build(message_params)
 
     respond_to do |format|
       if @message.save
@@ -36,7 +29,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /messages/1 or /messages/1.json
   def update
     respond_to do |format|
       if @message.update(message_params)
@@ -49,7 +41,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  # DELETE /messages/1 or /messages/1.json
   def destroy
     @message.destroy
     respond_to do |format|
@@ -60,12 +51,10 @@ class MessagesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_message
     @message = Message.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def message_params
     params.require(:message).permit(:body)
   end
