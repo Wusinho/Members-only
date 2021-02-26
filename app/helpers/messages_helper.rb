0e1dfa2b7c76ea8c67fbef33a27e0a468ceec686
Heber <heberlazo@gmail.com>
2021-02-26 14:53:18 -0500

    Fix linters

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,60 +1,37 @@
 module MessagesHelper
-
-def appNotice
-  if flash[:notice]
-    content_tag(:div, flash[:notice], class:["alert", "alert-info", "mt-0" ])
-  end
-end
-
-def appAlert
-
-  if flash[:alert] 
-    content_tag(:div, flash[:alert], class:["alert", "alert-info", "mt-0" ])
-   end 
-
+  def notice
+    content_tag(:div, flash[:notice], class: %w[alert alert-info mt-0]) if flash[:notice]
   end
 
-def ifNavabar
-  if user_signed_in?
-    link_to('Sign Out', destroy_user_session_path, method: :delete, class: "nav-link" ) +
-    content_tag(:a, current_user.username, class:["nav-link", "disabled" ])
-  else
-   link_to('Sign In', user_session_path, class: "nav-link active"  )
+  def alert
+    content_tag(:div, flash[:alert], class: %w[alert alert-info mt-0]) if flash[:alert]
   end
-end
 
-def ifFeed(message)
-  if user_signed_in?
-    content_tag(:div, message.user.username, class:"card-header" )
-  else
-    "@Hidden-guY"
-  end 
-end
+  def navbar
+    if user_signed_in?
+      link_to('Sign Out', destroy_user_session_path, method: :delete, class: 'nav-link') +
+        content_tag(:a, current_user.username, class: %w[nav-link disabled])
+    else
+      link_to('Sign In', user_session_path, class: 'nav-link active')
+    end
+  end
 
-def propertiesFEED(message)
-  if user_signed_in? and current_user.id == message.user_id
+  def feed(message)
+    if user_signed_in?
+      content_tag(:div, message.user.username, class: 'card-header')
+    else
+      '@Hidden-guY'
+    end
+  end
 
-      link_to('Edit', edit_message_path(message) )  + 
-      link_to('Destroy', message, method: :delete, data: { confirm: 'Are you sure?' } ) 
-    
-    
- 
-  end 
+  def properties(message)
+    return unless user_signed_in? and current_user.id == message.user_id
 
-end
+    link_to('Edit', edit_message_path(message)) +
+      link_to('Destroy', message, method: :delete, data: { confirm: 'Are you sure?' })
+  end
 
-def feedSigned
-
-if user_signed_in?
-  
-
- render 'form'
-
-end
-end
-
-
-
-
-
+  def signed
+    render 'form' if user_signed_in?
+  end
 end
